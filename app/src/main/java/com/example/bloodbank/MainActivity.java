feb478657b9a9185fdf46cf3d64836ce5498d541
As-Rahman <asrahman111@gmail.com>
2023-08-30 22:57:35 +0600

    Renamed sign in page name

--- app/src/main/java/com/example/bloodbank/MainActivity.java
+++ app/src/main/java/com/example/bloodbank/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_in);
 
         TextView username = (TextView) findViewById(R.id.username);
         TextView password = (TextView) findViewById(R.id.password) ;
