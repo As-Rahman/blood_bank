d144398364d336a7f17262380353187f4d8bcb47
As-Rahman <asrahman111@gmail.com>
2023-09-06 21:11:14 +0600

    Added updated activities,adapters,fragments,viewmodels

---
+++ app/src/main/java/com/example/bloodbank/fragments/pbar.java
@@ -0,0 +1,27 @@
+package com.example.bloodbank.fragments;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.asrahman.bloodbank.R;
+
+public class pbar extends AppCompatActivity {
+
+    Dialog dialog;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(R.layout.activity_progressbar);
+        // This should be called once in your Fragment's onViewCreated() or in Activity onCreate() method to avoid dialog duplicates.
+        dialog = builder.create();
+    }
+
+}
